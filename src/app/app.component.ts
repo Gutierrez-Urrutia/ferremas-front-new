9b3e72e9ac8a917548161f95b48ddeff86154377
Pablo Gutiérrez <pa.gutierrezu@duocuc.cl>
2025-05-25 01:15:44 -0400

    se agrega seccion de confianza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { NavbarComponent } from "./components/navbar/navbar.component";
 import { CarrouselComponent } from "./components/carrousel/carrousel.component";
 import { DestacadosComponent } from "./components/destacados/destacados.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { PrefooterComponent } from "./components/prefooter/prefooter.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, CarrouselComponent, DestacadosComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, CarrouselComponent, DestacadosComponent, FooterComponent, PrefooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
