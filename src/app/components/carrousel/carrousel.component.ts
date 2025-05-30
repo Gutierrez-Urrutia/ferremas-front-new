47ff800911dd3792a939d32cd0a9eddd8acfe201
Pablo Gutiérrez <pa.gutierrezu@duocuc.cl>
2025-05-30 05:52:22 -0400

    se agrega texto en hero-section; se agregan imagenes de productos

--- src/app/components/carrousel/carrousel.component.ts
+++ src/app/components/carrousel/carrousel.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-carrousel',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './carrousel.component.html',
   styleUrl: './carrousel.component.css'
 })
-export class CarrouselComponent {
+export class CarrouselComponent implements AfterViewInit {
 
+  ngAfterViewInit() {
+    // Inicializar el carrusel manualmente
+    const carouselElement = document.getElementById('carouselExampleCaptions');
+    if (carouselElement) {
+      const carousel = new bootstrap.Carousel(carouselElement, {
+        interval: 8000,
+        ride: 'carousel'
+      });
+    }
+  }
 }
