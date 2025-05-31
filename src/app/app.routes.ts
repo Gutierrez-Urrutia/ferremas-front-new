adb31403638351840c98959f63fcacbaaa395fb0
Pablo Gutiérrez <pa.gutierrezu@duocuc.cl>
2025-05-31 01:56:08 -0400

    se agregan paginas de login y registro; dropdown de usuario en navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,7 @@ export const routes: Routes = [
     { path: 'categoria/:id', loadComponent: () => import('./pages/categoria-producto/categoria-producto.component').then(m => m.CategoriaProductoComponent) },
     { path: 'marca/:id', loadComponent: () => import('./pages/marca-producto/marca-producto.component').then(m => m.MarcaProductoComponent) },
     { path: 'ofertas', loadComponent: () => import('./pages/oferta-producto/oferta-producto.component').then(m => m.OfertaProductoComponent) },
+    { path: 'login', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
+    { path: 'registro', loadComponent: () => import('./pages/registro/registro.component').then(m => m.RegistroComponent) },
     { path: '**', redirectTo:''}
 ];
